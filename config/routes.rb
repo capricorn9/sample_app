2b77b8a77d6c051d1b4207bebe804bbf60f50166
Dai Au <dailinhau@gmail.com>
2011-11-17 23:05:25 +0000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
+  
+   match '/signup',   :to => 'users#new'
 
-  get "pages/contact"
+match '/contact',  :to => 'pages#contact'
 
-  get "pages/about"
+#get "pages/about"
+#get "pages/home"
+#get "pages/help"
+
+match '/about',    :to => 'pages#about'
+match '/help',     :to => 'pages#help'
+
+root :to => 'pages#home'
+
+#about_path => '/about'
+#about_url => 'http://localhost:3000/about'
+
+#get "pages/home"
+
+#get "pages/contact"
+
+#get "pages/about"
+
+#get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,18 +3,24 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    #
+    @base_title = "Ruby on Rails Tutorial Sample App"
+    #
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
- 
+    
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
-     end
-   end
+                                    :content => @base_title + " | Home")
+    end
+  end
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -25,8 +31,8 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
-    end  
+                                    :content => @base_title + " | Contact")
+    end
   end
 
   describe "GET 'about'" do
@@ -38,7 +44,14 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be sucessful" do
+      get 'help'
+      response.should be_success
     end
   end
 end
